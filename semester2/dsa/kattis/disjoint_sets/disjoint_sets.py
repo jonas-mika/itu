80741c7894b72d00fbac534f7464b5606326ce0e
Jonas-Mika Senghaas <accounts@jonassenghaas.de>
2021-02-03 12:47:33 +0100

    Initial Commit MA Disjoint Sets

---
+++ semester2/dsa/kattis/disjoint_sets/disjoint_sets.py
@@ -0,0 +1,55 @@
+from itu.algs4.fundamentals.uf import WeightedQuickUnionUF
+import sys
+
+# read input 
+lines = sys.stdin.readlines()
+n, m = [int(i) for i in lines[0].split()]
+operations = [[int(i) for i in operation.split()] for operation in lines[1:]]
+
+# print(n,m)
+# print(operations)
+
+# my_operations = [
+#     [1,0,1],
+#     [1,1,2],
+#     [1,4,3],
+#     [2,4,2],
+#     [2,5,4],
+#     [2,0,3]
+# ]
+
+def main():
+    # initialising family of disjoints sets, as singelton {0}, {1}, ..., {n-1}
+    UF = WeightedQuickUnionUF(n)
+
+    for operation in operations:
+        o, s, t = operation
+        #print(UF._count) # number of disjoint sets
+        #print(UF._size) # list of sizes of each tree
+
+        if o == 0: # query
+            if UF.connected(s,t) == True:
+                print("1")
+            else: print("0")
+
+        elif o == 1: # union
+            UF.union(s,t)
+
+        elif o == 2: # move
+            # find children and parent
+            children = [index for index,elm in enumerate(UF._parent) if elm==s]
+            parent = UF.find(s)
+
+            if s == parent: # moving node is root node
+                children.remove(s)
+                for child in children:
+                    UF._parent[child] = children[0]
+            else: # moving node is not a root node
+                for child in children:
+                    UF._parent[child] = parent
+
+            # move s into t
+            UF._parent[s] = UF.find(t)
+
+if __name__ == '__main__':
+    main()
